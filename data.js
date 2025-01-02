02606c6ac202fd03e3c5ed1b761708345ec4b07e
reedsans <oladsposanusi@gmail.com>
2025-01-02 14:14:21 +0100

    First commit

---
+++ data.js
@@ -0,0 +1,58 @@
+export const tweetsData = [   
+    {
+        handle: `@GetSpammed607552 💎`,
+        profilePic: `images/troll.jpg`,
+        likes: 27,
+        retweets: 10,
+        tweetText: `Invest in Crypto Make 💰💰💰 low low prices. 
+            Guaranteed return on investment. HMU DMs open!!`,
+        replies: [],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '4b161eee-c0f5-4545-9c4b-8562944223ee',
+    },    
+    {
+        handle: `@Elon ✅`,
+        profilePic: `images/musk.png`,
+        likes: 4761,
+        retweets: 434,
+        tweetText: `I need volunteers for a one-way mission to Mars 🪐. No experience necessary🚀`,
+        replies: [
+                  {
+                handle: `@TomCruise ✅`,
+                profilePic: `images/tcruise.png`,
+                tweetText: `Yes! Sign me up! 😎🛩`,
+            },
+                  {
+                handle: `@ChuckNorr ✅`,
+                profilePic: `images/chucknorris.jpeg`,
+                tweetText: `I went last year😴`,
+            },
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '3c23454ee-c0f5-9g9g-9c4b-77835tgs2',
+    },
+        {
+        handle: `@HelloWorlder19`,
+        profilePic: `images/flower.png`,
+        likes: 7,
+        retweets: 3,
+        tweetText: `Can I be a coder without a computer?`,
+        replies: [
+            {
+                handle: `@ChatGPTuser ☣️`,
+                profilePic: `images/overflow.png`,
+                tweetText: `No. Of course not. Give up and find another career.`,
+            },
+            {
+                handle: `@Love&Light`,
+                profilePic: `images/love.png`,
+                tweetText: `You're wonderful just as you are! ❤️`,
+            },
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '8hy671sff-c0f5-4545-9c4b-1237gyys45',
+    },     
+]
